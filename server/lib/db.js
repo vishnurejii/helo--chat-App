53e33882fd9f54d25e262f6161792c1eef420f23
vishnurejii <vishnurejii2003@gmail.com>
2026-03-01 21:28:10 +0530

    first commit

---
+++ server/lib/db.js
@@ -0,0 +1,15 @@
+// import mongoose from "mongoose";
+// import { log } from "three/src/utils.js";
+
+// //function to connect to the mongodb database
+// export const connectSB = async () =>{
+//     try{
+//         mongoose.connection.on('connected',()=>console.log("Database connected"))
+
+//         await mongoose.connect(`&{process.env.MONGODB_URI}/caht-app`)
+//     }
+//     catch(error){
+//         console.log(error);
+        
+//     }
+// }
---
+++ server/server.js
@@ -0,0 +1,21 @@
+// import express from "express"
+// import "dotenv/config"
+// import cors from "cors"
+// import http from "http"
+
+// //create express app and http server
+
+// const app=express()
+// const server=http.createServer(app)
+
+// //middleware setup
+
+// app.use(express.json({limit: "4mb"}))
+// app.use(cors())
+
+
+// app.use("/api/status",(req,res)=>res.send("server is live"))
+
+
+// const PORT=process.env.PORT || 5000
+// server.listen(PORT, ()=> console.log("server is running on PORT: "+ PORT))
