2b99ef87d6a9230ee813db6b597af81af0590aab
vishnurejii <vishnurejii2003@gmail.com>
2026-03-01 21:30:43 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-// import express from "express"
-// import "dotenv/config"
-// import cors from "cors"
-// import http from "http"
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import http from "http"
 
-// //create express app and http server
+//create express app and http server
 
-// const app=express()
-// const server=http.createServer(app)
+const app=express()
+const server=http.createServer(app)
 
-// //middleware setup
+//middleware setup
 
-// app.use(express.json({limit: "4mb"}))
-// app.use(cors())
-
-
-// app.use("/api/status",(req,res)=>res.send("server is live"))
+app.use(express.json({limit: "4mb"}))
+app.use(cors())
 
 
-// const PORT=process.env.PORT || 5000
-// server.listen(PORT, ()=> console.log("server is running on PORT: "+ PORT))
+app.use("/api/status",(req,res)=>res.send("server is live"))
+
+
+const PORT=process.env.PORT || 5000
+server.listen(PORT, ()=> console.log("server is running on PORT: "+ PORT))
